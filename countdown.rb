bf7d3fd99892b896cd7e54f1ebb3ceb9b15852bb
git_mitchelleokemwa <mitchelleokemwa@gmail.com>
2023-02-19 14:32:16 +0300

    Completed assignment

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,35 @@
 #write your code here
 
-def countdown
+# def countdown
+#   countdown_output = 10
+#     until countdown_output == 0
+#     puts "#{countdown_output}SECOND(S)!"
+#     countdown_output -=1
+#     end
+#
+#     return "HAPPY NEW YEAR!"
+# end
+
+# def countdown
+# i = 10
+# while i > 0
+#   pp "#{i} SECOND(S)!"
+#   i -= 1
+# end
+# pp "HAPPY NEW YEAR!"
+# end
+def countdown(num)
+  while num > 0
+    puts "#{num} SECOND(S)!"
+    num -= 1
+  end
+  "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(n)
+  n.downto(0) do |i|
+    puts i
+    sleep 1
+  end
+end
+
